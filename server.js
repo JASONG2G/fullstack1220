e572a718da59cdb9672b7745e2f9aaa1f54cae90
jason <workstation@jasons-MacBook-Pro.local>
2020-12-11 14:24:31 -0600

    separate routes from server.js

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const expressLayouts = require("express-ejs-layouts");
+const indexRouter = require("./routes/index"); // get router from index.js 
 
 app.set("view engine", "ejs"); // set view engine to ejs.
 app.set("views", __dirname + "/views"); // all views come from ./views folder.
 app.set("layout", "layouts/layout"); // hook up express-layout.
 
 app.use(expressLayouts);
-app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(express.static("public")); // public folder is where html and css are at.
+app.use("/", indexRouter);
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
